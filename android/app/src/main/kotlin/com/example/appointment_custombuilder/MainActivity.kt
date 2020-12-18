9ce18884c1eaf5cd5181340278b6a92d5c0340b8
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-18 16:05:10 +0530

    FLUT-3198 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/appointment_custombuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appointment_custombuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(AppointmentBuilder());
+
+class AppointmentBuilder extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: SfCalendar(
+          view: CalendarView.week,
+          allowedViews: <CalendarView>[
+            CalendarView.day,
+            CalendarView.workWeek,
+            CalendarView.week,
+            CalendarView.month,
+            CalendarView.timelineDay,
+            CalendarView.timelineWeek,
+            CalendarView.timelineWorkWeek,
+            CalendarView.timelineMonth,
+            CalendarView.schedule
+          ],
+          dataSource: _getCalendarDataSource(),
+          monthViewSettings: MonthViewSettings(showAgenda: true),
+          timeSlotViewSettings:
+              TimeSlotViewSettings(timelineAppointmentHeight: 100),
+          appointmentBuilder: appointmentBuilder,
+        )),
+      ),
+    );
+  }
+
+  _AppointmentDataSource _getCalendarDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+
+    appointments.add(Appointment(
+      startTime: DateTime(
+        2020,
+        12,
+        15,
+        7,
+        0,
+        0,
+      ),
+      endTime: DateTime(2020, 12, 15, 11, 0, 0),
+      subject: 'Meeting',
+      color: Colors.pink,
+    ));
+    return _AppointmentDataSource(appointments);
+  }
+
+  Widget appointmentBuilder(BuildContext context,
+      CalendarAppointmentDetails calendarAppointmentDetails) {
+    final Appointment appointment =
+        calendarAppointmentDetails.appointments.first;
+    return Column(
+      children: [
+        Container(
+            width: calendarAppointmentDetails.bounds.width,
+            height: calendarAppointmentDetails.bounds.height / 2,
+            color: appointment.color,
+            child: Center(
+              child: Icon(
+                Icons.group,
+                color: Colors.black,
+              ),
+            )),
+        Container(
+          width: calendarAppointmentDetails.bounds.width,
+          height: calendarAppointmentDetails.bounds.height / 2,
+          color: appointment.color,
+          child: Text(
+            appointment.subject +
+                DateFormat(' (hh:mm a').format(appointment.startTime) +
+                '-' +
+                DateFormat('hh:mm a)').format(appointment.endTime),
+            textAlign: TextAlign.center,style: TextStyle(fontSize: 10),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
